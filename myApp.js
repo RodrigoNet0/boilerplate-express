4db9fbae6cb1917a3c176e3cd40c7e5fc60405e0
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-01-26 14:11:26 +0000

    feito

--- myApp.js
+++ myApp.js
@@ -2,9 +2,21 @@ let express = require('express');
 let app = express();
 
 
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
+
+  app.listen(3000, function(){console.log('Rodando')});
 
 
 
+// get => pedir info
+// post => receber info
+// put => atulizar info
+// delete => é obvio
+// options => informações sobre api (requisição)
 
 
 
